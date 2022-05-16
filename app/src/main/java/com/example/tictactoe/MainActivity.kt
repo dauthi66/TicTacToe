976dac81cfc7a7d8e2ed8b7ba7e955adf9e416e4
Kris <dauthi66@gmail.com>
2022-05-15 19:01:57 -0700

    Changed a majority of hardcoded text to resource strings. Implemented logic to check for match winners.

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -14,8 +14,8 @@ class MainActivity : AppCompatActivity() {
         val  newGameButton = findViewById<Button>(R.id.new_game)
         setupNewGameButton(newGameButton)
 
-        //setup all buttons
         //find buttons
+
         val  button1 = findViewById<Button>(R.id.button1)
         val  button2 = findViewById<Button>(R.id.button2)
         val  button3 = findViewById<Button>(R.id.button3)
@@ -26,11 +26,15 @@ class MainActivity : AppCompatActivity() {
         val  button8 = findViewById<Button>(R.id.button8)
         val  button9 = findViewById<Button>(R.id.button9)
 
-        val buttons = arrayOf(button1, button2, button3, button4, button5, button6,
-                                button7, button8, button9)
-        buttons.forEach {
-            setupOnClick(it)
-        }
+        //create 2d array of buttons to look for rows
+        val buttonRows = arrayOf(
+            arrayOf(button1, button2, button3),
+            arrayOf(button4, button5, button6),
+            arrayOf(button7, button8, button9)
+        )
+
+        //setup all buttons
+        setupOnClick(buttonRows)
     }
 
     private fun setupNewGameButton(newGameButton: Button?) {
@@ -39,20 +43,92 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun setupOnClick(button: Button?) {
+    private fun setupOnClick(buttonRows : Array<Array<Button>>) {
+        //find player turn indicator
         val playerTurn = findViewById<TextView>(R.id.textView)
 
-        button?.setOnClickListener {
-            if (playerTurn.text == getString(R.string.playerX)) {
-                button.text = "X"
+        //setup player strings
+        val playerOneMarker = getString(R.string.player_one_marker)
+        val playerOne = getString(R.string.playerTurn, playerOneMarker)
+        val playerTwoMarker = getString(R.string.player_two_marker)
+        val playerTwo = getString(R.string.playerTurn, playerTwoMarker)
 
-                playerTurn.text = getString(R.string.playerO)
-            } else {
-                button.text = "O"
-                playerTurn.text = getString(R.string.playerX)
+        //create win combinations from 2d array
+        val buttonColumns = createButtonColumns(buttonRows)
+        val buttonDiagonals = createButtonDiagonals(buttonRows)
+
+        //set to first player's turn
+        playerTurn.text = playerOne
+
+        //setup button turn logic
+        for (arrays in buttonRows) {
+            for (button in arrays) {
+                button.setOnClickListener {
+                    if (playerTurn.text == playerOne) {
+                        button.text = playerOneMarker
+
+                        //switch turns
+                        playerTurn.text = playerTwo
+                    }
+                    else {
+                        button.text = playerTwoMarker
+
+                        //switch turns
+                        playerTurn.text = playerOne
+                    }
+
+                    //disable button after use
+                    button.isClickable = false
+
+                    //check for player1 wins
+                    checkWin(buttonRows, playerOneMarker)
+                    checkWin(buttonColumns, playerOneMarker)
+                    checkWin(buttonDiagonals, playerOneMarker)
+                    //check for player 2 wins
+                    checkWin(buttonRows, playerTwoMarker)
+                    checkWin(buttonColumns, playerTwoMarker)
+                    checkWin(buttonDiagonals, playerTwoMarker)
+
+                    //TODO: implement tie logic
+                    //TODO: lock game on win
+                }
             }
-
-            button.isClickable = false
         }
+
+    }
+
+    private fun checkWin(buttons : Array<Array<Button>>, player : String): Boolean {
+        for (arrays in buttons) {
+            //reset on new row
+            var matchCount = 0
+            for (button in arrays) {
+                //tally X's in row
+                if (button.text == player) {
+                    matchCount++
+                    if (matchCount == 3) {
+                        val playerTurn = findViewById<TextView>(R.id.textView)
+                        playerTurn.text = "Player " + player + " WINS!" //TODO: remove hardcode
+                        return true
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private fun createButtonColumns(buttonRows : Array<Array<Button>>) : Array<Array<Button>> {
+       return arrayOf(
+            arrayOf(buttonRows[0][0], buttonRows[1][0], buttonRows[2][0]),
+            arrayOf(buttonRows[0][1], buttonRows[1][1], buttonRows[2][1]),
+            arrayOf(buttonRows[0][2], buttonRows[1][2], buttonRows[2][2])
+        )
+    }
+
+
+    private fun createButtonDiagonals(buttons: Array<Array<Button>>): Array<Array<Button>> {
+        return arrayOf(
+            arrayOf(buttons[0][0], buttons[1][1], buttons[2][2]),
+            arrayOf(buttons[0][2], buttons[1][1], buttons[2][0]),
+        )
     }
 }
