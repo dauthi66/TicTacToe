d79a475000f3bc382db04ef7746c0fee5df9fa55
Kris <dauthi66@gmail.com>
2022-05-17 19:15:27 -0700

    Fixed tie logic and some buttons not disabling. Cleaned up code, and refactored to increase efficiency skipping running code when uneeded.

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -12,10 +12,8 @@ class MainActivity : AppCompatActivity() {
 
         //setup new game
         val  newGameButton = findViewById<Button>(R.id.new_game)
-        setupNewGameButton(newGameButton)
 
         //find buttons
-
         val  button1 = findViewById<Button>(R.id.button1)
         val  button2 = findViewById<Button>(R.id.button2)
         val  button3 = findViewById<Button>(R.id.button3)
@@ -26,6 +24,12 @@ class MainActivity : AppCompatActivity() {
         val  button8 = findViewById<Button>(R.id.button8)
         val  button9 = findViewById<Button>(R.id.button9)
 
+        //find player turn indicator
+        val playerTurn = findViewById<TextView>(R.id.textView)
+        //setup playerOne strings
+        val playerOneMarker = getString(R.string.player_one_marker)
+        val playerOne = getString(R.string.playerTurn, playerOneMarker)
+
         //create 2d array of buttons to look for rows
         val buttonRows = arrayOf(
             arrayOf(button1, button2, button3),
@@ -33,23 +37,32 @@ class MainActivity : AppCompatActivity() {
             arrayOf(button7, button8, button9)
         )
 
-        //setup all buttons
-        setupOnClick(buttonRows)
+        //setup new game button function
+        setupNewGameButton(newGameButton, buttonRows, playerOne, playerTurn)
+        //setup all button logic
+        setupOnClick(buttonRows, playerOne, playerOneMarker, playerTurn)
     }
 
-    private fun setupNewGameButton(newGameButton: Button?) {
+    private fun setupNewGameButton
+                (newGameButton: Button?, buttonRows : Array<Array<Button>>,
+                playerOne : String, playerTurn : TextView ) {
         newGameButton?.setOnClickListener {
-            recreate()
+            playerTurn.text = playerOne
+            for (arrays in buttonRows) {
+                for (button in arrays) {
+                    if (!button.isClickable) {
+                        button.isClickable = true
+                    }
+                    button.text = ""
+                }
+            }
         }
     }
 
-    private fun setupOnClick(buttonRows : Array<Array<Button>>) {
-        //find player turn indicator
-        val playerTurn = findViewById<TextView>(R.id.textView)
-
-        //setup player strings
-        val playerOneMarker = getString(R.string.player_one_marker)
-        val playerOne = getString(R.string.playerTurn, playerOneMarker)
+    private fun setupOnClick
+                (buttonRows : Array<Array<Button>>, playerOne : String,
+                playerOneMarker : String, playerTurn : TextView ) {
+        //setup player 2 strings
         val playerTwoMarker = getString(R.string.player_two_marker)
         val playerTwo = getString(R.string.playerTurn, playerTwoMarker)
 
@@ -67,61 +80,67 @@ class MainActivity : AppCompatActivity() {
                     if (playerTurn.text == playerOne) {
                         button.text = playerOneMarker
 
-                        //switch turns
                         playerTurn.text = playerTwo
                     }
                     else {
                         button.text = playerTwoMarker
 
-                        //switch turns
                         playerTurn.text = playerOne
                     }
 
                     //disable button after use
                     button.isClickable = false
 
+                    var gameOver = false
                     //check for player1 wins
-                    checkWinOrTie(buttonRows, playerOneMarker)
-                    checkWinOrTie(buttonColumns, playerOneMarker)
-                    checkWinOrTie(buttonDiagonals, playerOneMarker)
-                    //check for player 2 wins
-                    checkWinOrTie(buttonRows, playerTwoMarker)
-                    checkWinOrTie(buttonColumns, playerTwoMarker)
-                    checkWinOrTie(buttonDiagonals, playerTwoMarker)
+                    gameOver = isWinOrTie(buttonDiagonals, playerOneMarker, playerTurn, gameOver)
+                    gameOver = isWinOrTie(buttonRows, playerOneMarker, playerTurn, gameOver)
+                    gameOver = isWinOrTie(buttonColumns, playerOneMarker, playerTurn, gameOver)
 
-                    //TODO: make it so you can't flip the screen
+                    //check for player 2 wins
+                    gameOver = isWinOrTie(buttonDiagonals, playerTwoMarker, playerTurn, gameOver)
+                    gameOver = isWinOrTie(buttonRows, playerTwoMarker, playerTurn, gameOver)
+                               isWinOrTie(buttonColumns, playerTwoMarker, playerTurn, gameOver)
                 }
             }
         }
     }
 
-    private fun checkWinOrTie(buttons : Array<Array<Button>>, player : String) {
-        //find turn display
-        val playerTurn = findViewById<TextView>(R.id.textView)
+    private fun isWinOrTie
+                (buttons : Array<Array<Button>>, player : String,
+                 playerTurn : TextView, gameOver: Boolean) :  Boolean{
         //track used buttons
         var usedButtons = 0
 
-        for (arrays in buttons) {
-            //reset on new row
-            var matchCount = 0
-            for (button in arrays) {
-                if (button.text.isNotEmpty()) {
-                    usedButtons++
-                }
-                //tally marked buttons in row
-                if (button.text == player) {
-                    matchCount++
-                    if (matchCount == 3) {
-                        playerTurn.text = getString(R.string.win_game, player)
-                        disableButtons(buttons)
+        //skip if game is over
+        if (!gameOver) {
+            for (arrays in buttons) {
+                //reset on new row
+                var matchCount = 0
+                for (button in arrays) {
+                    if (button.text.isNotBlank()) {
+                        usedButtons++
+                    }
+                    //tally marked buttons in row
+                    if (button.text == player) {
+                        matchCount++
+                        if (matchCount == 3) {
+                            playerTurn.text = getString(R.string.win_game, player)
+                            return true
+                        }
                     }
                 }
+                //check for tie game (all buttons used)
+                if (usedButtons == 9) {
+                    playerTurn.text = getString(R.string.tie_game)
+                }
             }
-            //check for tie game (all buttons used)
-            if (usedButtons == buttons.size * buttons[0].size) {
-                playerTurn.text = getString(R.string.tie_game)
-            }
+            return false
         }
+        else {
+            disableButtons(buttons)
+        }
+        return true
     }
 
     private fun createButtonColumns(buttonRows : Array<Array<Button>>) : Array<Array<Button>> {
@@ -131,7 +150,6 @@ class MainActivity : AppCompatActivity() {
             arrayOf(buttonRows[0][2], buttonRows[1][2], buttonRows[2][2])
         )
     }
-
 
     private fun createButtonDiagonals(buttonRows: Array<Array<Button>>): Array<Array<Button>> {
         return arrayOf(
